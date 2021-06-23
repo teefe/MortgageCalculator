521ab132df746f8c32a52277ccc9ffad784f3c0f
Derrick Teefe <70985060+teefe@users.noreply.github.com>
2021-06-23 03:52:49 -0700

    The Class responsible for the report

---
+++ MortgageReport.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class MortgageReport {
+
+    public void presentation(String input1, String input2){
+        System.out.println();
+        System.out.println(input1);
+        System.out.println("---------");
+        System.out.print( input2);
+    }
+
+
+}
